dbc33ad474bf0665b0e7ab232b80868ae43dc7b4
Jonathan Mancia <jonathanman2010@icloud.com>
2017-02-03 12:01:17 -0700

    V1.0.0

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var ejs = require('ejs');
+
+app.get('/', function(request, response){
+	response.sendFile(__dirname + '/index.html');
+});
+app.get('/courses', function(request, response){
+	fs.readFile('courses.json', 'utf8', function(err, data){
+		var courses = JSON.parse(data);
+			response.locals = { courses: courses };
+			response.render('courses.ejs');
+	});
+});
+app.get('/courses/:id', function(request, response){
+	fs.readFile('courses.json', 'utf8', function(err, data){
+		var coursesParsed = JSON.parse(data);
+		var course = coursesParsed.filter( function(obj){
+			return obj.id === parseInt(request.params.id);
+		})[0];
+
+		response.locals = { course: course }
+		response.render('course.ejs');
+	})
+})
+app.listen(8080);
+console.log('Success!')
